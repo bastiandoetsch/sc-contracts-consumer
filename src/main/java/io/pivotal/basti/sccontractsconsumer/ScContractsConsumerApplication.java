a9f9171c3af988ce8cbcf5ad27f14763c7713ac3
Bastian Doetsch <bdoetsch@pivotal.io>
2020-03-10 17:12:26 +0100

    init

---
+++ src/main/java/io/pivotal/basti/sccontractsconsumer/ScContractsConsumerApplication.java
@@ -0,0 +1,13 @@
+package io.pivotal.basti.sccontractsconsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScContractsConsumerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScContractsConsumerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/pivotal/basti/sccontractsconsumer/ScContractsConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.pivotal.basti.sccontractsconsumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScContractsConsumerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
